4a99a356020d8ddf2483d98475064f29aae84cfc
nahirlopez <101569242+nahirlopez@users.noreply.github.com>
2022-04-07 23:29:57 -0300

    video 1 2 y 3

---
+++ Assets/Scenes/Scripts/MayoriaDeEdad.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MayoriaDeEdad : MonoBehaviour
+{
+    // Start is called before the first frame update
+    
+    
+    public int edad = 16;
+    public Text mitexto;
+    void Start()
+    {
+        
+        if (edad>= 18)
+        {
+            mitexto.text = "Es mayor de edad";
+            Debug.Log("Es mayor de edad");
+        }
+        else
+        {
+            mitexto.text = "No es mayor de edad";
+            Debug.Log("No es mayor de edad");
+        }
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
