6f84820ad128c61a546c6b3578ccaef286d5bea9
nahirlopez <101569242+nahirlopez@users.noreply.github.com>
2022-04-08 00:23:21 -0300

    terminado

--- Assets/Scenes/Contraseña/Scripts/ValidarContraseña.cs
+++ Assets/Scenes/Contraseña/Scripts/ValidarContraseña.cs
@@ -10,10 +10,13 @@ public class ValidarContraseña : MonoBehaviour
     string contraseñaCorrecta;
     string ContraseñaUsuario;
     public Text ingresoUsuario;
+    public Text textomsj;
+    public GameObject cartelitoMsj;
 
     void Start()
     {
         contraseñaCorrecta = "12345";
+        cartelitoMsj.SetActive(false);
     }
 
     // Update is called once per frame
@@ -29,10 +32,14 @@ public class ValidarContraseña : MonoBehaviour
         if (contraseñaCorrecta == ContraseñaUsuario)
         {
             Debug.Log("Bienvenido");
+            cartelitoMsj.SetActive(true);
+            textomsj.text = "Bienvenido";
         }
         else
         {
+            cartelitoMsj.SetActive(true);
             Debug.Log("Contraseña incorrecta");
+            textomsj.text = "Contraseña incorrecta";
         }
     }
 }
