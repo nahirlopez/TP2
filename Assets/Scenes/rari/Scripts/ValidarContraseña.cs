8f4a05561af2d2ad46d6ec8cfd2dbc85198c44de
nahirlopez <101569242+nahirlopez@users.noreply.github.com>
2022-04-08 00:11:11 -0300

    video 4 y 5

---
+++ Assets/Scenes/rari/Scripts/ValidarContraseña.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ValidarContraseña : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    string contraseñaCorrecta;
+    string ContraseñaUsuario;
+    public Text ingresoUsuario;
+
+    void Start()
+    {
+        contraseñaCorrecta = "12345";
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void validarContraseña()
+    {
+        ContraseñaUsuario = ingresoUsuario.text;
+        
+        if (contraseñaCorrecta == ContraseñaUsuario)
+        {
+            Debug.Log("Bienvenido");
+        }
+        else
+        {
+            Debug.Log("Contraseña incorrecta");
+        }
+    }
+}
